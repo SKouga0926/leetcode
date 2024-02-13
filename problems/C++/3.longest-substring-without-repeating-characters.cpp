ea23731862b0c180964a08585e70c91ec65eee45
SKouga0926 <sugarkouga0926@gmail.com>
2024-02-13 14:42:44 +0900

    fix leetcode no.3

--- problems/C++/3.longest-substring-without-repeating-characters.cpp
+++ problems/C++/3.longest-substring-without-repeating-characters.cpp
@@ -12,8 +12,8 @@ public:
         int max_len = 0;
         int left_ptr = 0;
         int s_length = s.length();
-        unordered_set<char> char_set;
-        
+        std::unordered_set<char> char_set;
+
 
         for (int right_ptr = 0; right_ptr < s_length; right_ptr++) {
 
@@ -35,7 +35,7 @@ public:
 
             if (char_set.count(s[right_ptr]) == 0) {
 
-                max_len = max(max_len, right_ptr - left_ptr +  1);
+                max_len = std::max(max_len, right_ptr - left_ptr +  1);
                 char_set.insert(s[right_ptr]);
             }
 
